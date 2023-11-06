904a0436c5f222e4668fc525b5edcef992ac43df
drmhmdkse <drmhmdkse@hotmail.com>
2023-11-06 18:26:18 +0300

    dal

---
+++ DAL/Concrete/EntityFramework/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Concrete.EntityFramework
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<DerslikEntity> Derslikler { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\sqlite3\\damyo.db");
+        }
+    }
+}
---
+++ Entities/Concrete/DerslikEntity.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public  class DerslikEntity : IEntity
+    {
+        public int ID { get; set; } 
+        public string Name { get; set; }
+        public string ShortCode { get; set; }
+        public int TotalHours { get; set; }
+        public string ColorHexCode { get; set; }
+    }
+}
